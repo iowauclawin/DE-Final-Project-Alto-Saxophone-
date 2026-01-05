38cda45bc205ce9ee5e53016526234a679296ab4
Hansol Lee <145624624+iowauclawin@users.noreply.github.com>
2026-01-05 15:37:50 -0800

    added some of the code for notes when a combination of buttons are pressed

--- button.py
+++ button.py
@@ -1,9 +1,70 @@
 import gpiozero
 
-button = Button(2)
+button1 = Button(2)
+button2 = Button(3)
+button3 = Button(4)
+button4 = Button(5)
+button5 = Button(6)
+button6 = Button(7)
+button7 = Button(8)
+button8 = Button(9)
+button9 = Button(10)
+button10 = Button(11)
+button11 = Button(12)
+button12 = Button(13)
+button13 = Button(14)
+button14 = Button(15)
+octave = Button(16)
 
 while True:
-    if button.is_pressed:
-        print("Button is pressed")
-    else:
-        print("Button is not pressed")
+    if not button1.is_pressed and not button2.is_pressed and not button3.is_pressed and not button4.is_pressed and not button5.is_pressed and 
+       not button6.is_pressed and not button7.is_pressed and not button8.is_pressed and button9.is_pressed and button10.is_pressed and
+       not button11.is_pressed and not button12.is_pressed and not button13.is_pressed and not button14.is_pressed and octave.is_pressed:
+        //play high d# or e flat
+    elif not button1.is_pressed and not button2.is_pressed and not button3.is_pressed and not button4.is_pressed and not button5.is_pressed and 
+       not button6.is_pressed and not button7.is_pressed and not button8.is_pressed and not button9.is_pressed and button10.is_pressed and
+       not button11.is_pressed and not button12.is_pressed and not button13.is_pressed and not button14.is_pressed and octave.is_pressed:
+        //play high d
+    elif not button1.is_pressed and not button2.is_pressed and not button3.is_pressed and not button4.is_pressed and not button5.is_pressed and 
+       not button6.is_pressed and not button7.is_pressed and not button8.is_pressed and not button9.is_pressed and not button10.is_pressed and
+       not button11.is_pressed and not button12.is_pressed and not button13.is_pressed and not button14.is_pressed and octave.is_pressed:
+        //play high c# or d flat
+    elif not button1.is_pressed and button2.is_pressed and not button3.is_pressed and not button4.is_pressed and not button5.is_pressed and 
+       not button6.is_pressed and not button7.is_pressed and not button8.is_pressed and not button9.is_pressed and not button10.is_pressed and
+       not button11.is_pressed and not button12.is_pressed and not button13.is_pressed and not button14.is_pressed and octave.is_pressed:
+        //play high c
+    elif button1.is_pressed and not button2.is_pressed and not button3.is_pressed and not button4.is_pressed and not button5.is_pressed and 
+       not button6.is_pressed and not button7.is_pressed and not button8.is_pressed and not button9.is_pressed and not button10.is_pressed and
+       not button11.is_pressed and not button12.is_pressed and not button13.is_pressed and not button14.is_pressed and octave.is_pressed:
+        //play high b
+    elif button1.is_pressed and button2.is_pressed and not button3.is_pressed and not button4.is_pressed and not button5.is_pressed and 
+       not button6.is_pressed and not button7.is_pressed and not button8.is_pressed and not button9.is_pressed and not button10.is_pressed and
+       not button11.is_pressed and not button12.is_pressed and not button13.is_pressed and not button14.is_pressed and octave.is_pressed:
+        //play high a
+    elif button1.is_pressed and button2.is_pressed and button3.is_pressed and not button4.is_pressed and not button5.is_pressed and 
+       not button6.is_pressed and not button7.is_pressed and not button8.is_pressed and not button9.is_pressed and not button10.is_pressed and
+       not button11.is_pressed and button12.is_pressed and not button13.is_pressed and not button14.is_pressed and octave.is_pressed:
+        //play high g# or a flat
+    elif button1.is_pressed and button2.is_pressed and button3.is_pressed and not button4.is_pressed and not button5.is_pressed and 
+       not button6.is_pressed and not button7.is_pressed and not button8.is_pressed and not button9.is_pressed and not button10.is_pressed and
+       not button11.is_pressed and not button12.is_pressed and not button13.is_pressed and not button14.is_pressed and octave.is_pressed:
+        //play high g
+    elif button1.is_pressed and button2.is_pressed and button3.is_pressed and not button4.is_pressed and button5.is_pressed and 
+       not button6.is_pressed and not button7.is_pressed and not button8.is_pressed and not button9.is_pressed and not button10.is_pressed and
+       not button11.is_pressed and not button12.is_pressed and not button13.is_pressed and not button14.is_pressed and octave.is_pressed:
+        //play f# or g flat
+    elif button1.is_pressed and button2.is_pressed and button3.is_pressed and button4.is_pressed and not button5.is_pressed and 
+       not button6.is_pressed and not button7.is_pressed and not button8.is_pressed and not button9.is_pressed and not button10.is_pressed and
+       not button11.is_pressed and not button12.is_pressed and not button13.is_pressed and not button14.is_pressed and octave.is_pressed:
+        //play f
+    elif button1.is_pressed and button2.is_pressed and button3.is_pressed and button4.is_pressed and button5.is_pressed and 
+       not button6.is_pressed and not button7.is_pressed and not button8.is_pressed and not button9.is_pressed and not button10.is_pressed and
+       not button11.is_pressed and not button12.is_pressed and not button13.is_pressed and not button14.is_pressed and octave.is_pressed:
+        //play e
+    elif button1.is_pressed and button2.is_pressed and button3.is_pressed and button4.is_pressed and button5.is_pressed and 
+        button6.is_pressed and button7.is_pressed and not button8.is_pressed and not button9.is_pressed and not button10.is_pressed and
+       not button11.is_pressed and not button12.is_pressed and not button13.is_pressed and not button14.is_pressed and octave.is_pressed:
+        //play e flat or d sharp
+    
+
+           
